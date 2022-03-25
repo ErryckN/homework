ddd4ef325592b4423f2a4100b10e6c121d3e3135
ErryckN <errycknorrys@gmail.com>
2022-03-25 13:55:27 +0700

    menggunakan component

---
+++ src/components/music.js
@@ -0,0 +1,11 @@
+const Music =() =>{
+    return (
+        <div>
+            <h1>Playlist</h1>
+            <h2>Title :</h2>
+            <h2>Artist :</h2>
+        </div>
+    )
+}
+
+export default Music;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,9 +14,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
