a7598de2f5edafd422db9bdb4818e8560be638bf
ErryckN <errycknorrys@gmail.com>
2022-03-28 14:34:16 +0700

    input table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Music from './components/music';
+//import Music from './components/music';
+import Table from './components/Table';
+//import list from './data/data.js';
+
+const list = [
+  {name: "A Night At The Opera (2011 Remaster)",
+      release_date: "1975-11-21",
+      release_date_precision: "day",
+      total_tracks: 12,
+      type: "album",
+      uri: "spotify:album:1GbtB4zTqAsyfZEsm1RZfx"}
+]
+
+const colNames = ['Name', 'Album', 'Artist']
 
 function App() {
   return (
     <div className='App'>
-      <header>
-        <Music/>
-      </header>
+      <Table list={list} colName={colNames}/>
     </div>
   )
 }
