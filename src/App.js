853f6ac581233b6a76f82624576565c18a168fef
ErryckN <errycknorrys@gmail.com>
2022-03-30 16:36:42 +0700

    add auth

--- src/App.js
+++ src/App.js
@@ -2,24 +2,77 @@
 import Table from './components/Table';
 //import list from './data/data.js';
 
-const list = [
-  {name: "A Night At The Opera (2011 Remaster)",
-      release_date: "1975-11-21",
-      release_date_precision: "day",
-      total_tracks: 12,
-      type: "album",
-      uri: "spotify:album:1GbtB4zTqAsyfZEsm1RZfx"}
-]
-
-const colNames = ['Name', 'Album', 'Artist']
-
 function App() {
-  return (
-    <div className='App'>
-      <Table list={list} colName={colNames}/>
-    </div>
-  )
-}
+  const [tracks, setTracks] = useState([]);
+  const [error, setError] = useState(null);
 
+  const handleAuthSpotify = () => {
+    authService.login();
+  };
+
+  const handleUnAuthSpotify = () => {
+    authService.logout();
+  };
+
+  const geTracks = async (query) => {
+    if (!query) return;
+    try {
+      const {
+        data: {
+          tracks: { items },
+        },
+      } = await trackService.search(query);
+
+      setTracks(items);
+      // eslint-disable-next-line no-shadow
+    } catch (error) {
+      let errorMessage = 'Something went wrong';
+      if (error.response && error.response.status === 401) {
+        errorMessage = 'Please login to Spotify';
+      } else {
+        errorMessage = error.response.data.error.message;
+      }
+      setError(errorMessage);
+    }
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    const query = event.target[0].value;
+    await geTracks(query);
+  };
+
+  return (
+    <div className="app">
+      <h1 className="title">Create Playlist</h1>
+      <PlaylistContainer />
+      <div className="header d-flex justify-between">
+        <h1>Spotify Playlist</h1>
+        {!isAuth() && (
+          <button
+            onClick={handleAuthSpotify}
+            className="btn btn-spotify"
+            type="button"
+          >
+            Auth to Spotify
+          </button>
+        )}
+        {isAuth() && (
+          <button
+            onClick={handleUnAuthSpotify}
+            className="btn btn-danger"
+            type="button"
+          >
+            Disconnect from Spotify
+          </button>
+        )}
+      </div>
+      <SearchBar onSearchChange={handleSearch} />
+      {!error && tracks.length > 0 && <PlaylistContainer tracks={tracks} />}
+      {!error && tracks.length === 0 && <p>No tracks found</p>}
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
 
 export default App;
